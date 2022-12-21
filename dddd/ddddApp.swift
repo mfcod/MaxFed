be71a16b07d39902bd9954d2e9106b78c73e696d
mfcodes <rinzlerr98@gmai.com>
2022-12-21 14:04:24 +0200

    Initial Commit

---
+++ dddd/ddddApp.swift
@@ -0,0 +1,17 @@
+//
+//  ddddApp.swift
+//  dddd
+//
+//  Created by Max on 21.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ddddApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
